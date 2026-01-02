f985ba21544124b67cfed4204dc78bb031571316
Soumendu1727 <sadhukhansoumendu@gmail.com>
2026-01-02 13:03:31 +0530

    Configure dynamic port for Render deployment

--- Program.cs
+++ Program.cs
@@ -2,6 +2,9 @@ using FinanceTracker.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+
 builder.Services.AddRazorPages();
 // builder.Services.AddSession(options =>
 // {
